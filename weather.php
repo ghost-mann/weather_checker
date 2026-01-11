f747346c5e0726665c68cf7777e36c4805239179
ghost-mann <iamaob8@protonmail.com>
2026-01-11 10:02:51 +0300

    feat: added styling in styles.css, added basic html in weather.php

--- weather.php
+++ weather.php
@@ -1,4 +1,7 @@
 <?php
+
+$city = $_POST["city"];
+
 ?>
 
 <!DOCTYPE html>
@@ -6,8 +9,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Weather Checker</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+
+<h1>Global Weather Checker</h1>
+
+<div class="container">
+    <form class="form-group">
+        <div class="form-group">
+            <label>Enter your City Below<br><input name="city" id="city" placeholder="Enter your city..."></label>
+        </div>
+        <div class="button-group">
+            <button name="submit" id="submit">Submit</button>
+        </div>
+    </form>
+</div>
 
 </body>
 </html>
