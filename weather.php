e60e2ae8887aafcd50d6461cdea32de57d26d218
ghost-mann <iamaob8@protonmail.com>
2026-01-12 08:57:24 +0300

    feat: added php code extracting transforming and displaying weather data in weather.php

--- weather.php
+++ weather.php
@@ -1,6 +1,34 @@
 <?php
 
-$city = $_POST["city"];
+// open weather api key
+$api_key = '7ae7cb7150334a07d2d55142ba25b041';
+
+// only run when form is submitted
+if (isset($_POST['city'])) {
+    $city = $_POST['city'];
+}
+
+$url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key&units=metric";
+
+$json = file_get_contents($url);
+
+$data = json_decode($json, true);
+
+if ($data && $data["cod"] == "200") {
+    $temp = $data["main"]["temp"];
+    $desc = $data["weather"][0]["description"];
+    $icon = $data["weather"][0]["icon"];
+
+    echo "<h2>Weather in $city</h2>";
+    echo "<p>The temperature is: $temp.</p>";
+    echo "<p>Today:$desc";
+    echo "<img src='https://openweathermap.org/img/wn/$icon@2x.png' alt='Weather icon'>";
+    } else {
+    echo "City not found!";
+}
+
+
+
 
 ?>
 
@@ -16,7 +44,7 @@ $city = $_POST["city"];
 <h1>Global Weather Checker</h1>
 
 <div class="container">
-    <form class="form-group">
+    <form class="form-group" method="post">
         <div class="form-group">
             <label>Enter your City Below<br><input name="city" id="city" placeholder="Enter your city..."></label>
         </div>
